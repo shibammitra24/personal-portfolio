0ec04bc591dabf11f597ebf8d0f092f68614dd77
shibammitra24 <shibammitra695@gmail.com>
2024-08-31 00:03:41 +0530

    Optimized SEO

---
+++ src/app/sitemap.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from "next";
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  return [
+    {
+      url: `${process.env.SITE_URL}`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${process.env.SITE_URL}/contact`,
+      lastModified: new Date(),
+    },
+  ];
+}
